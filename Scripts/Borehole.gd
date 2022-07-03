327e2ea314c49bac2111dc4a34f65d931ce35dc2
Ícaro Freire <icarovf@outlook.com>
2022-07-03 20:10:32 -0300

    updates

--- Scripts/Borehole.gd
+++ Scripts/Borehole.gd
@@ -1,13 +1,22 @@
 extends Node
 
+export var title = 'Borehole 1'
+
 # Physical properties
 export var depth: float				# in meters
 export var inner_radius: float		# in cm
 export var outer_radius: float		# in cm
 
+# Radiological parameters
+export var sources: int = 1
+export var activity: float = 10
+
 var casing = load('res://Prefabs/Casing.tscn')
+var gui = false
 
 func _ready():
+	$Control/Info/Title.text = self.title
+	
 	var n = int(depth / 100)
 	
 	for i in n:
@@ -20,4 +29,20 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	
+	if $Control.visible:
+		$Control/Info.rect_position = $Control.get_global_mouse_position() + Vector2.RIGHT * 24
+		
+		$Control/Info/Sources.text = str(self.sources)
+		$Control/Info/Radioactivity.text = str(self.activity)
+	
+	pass
+
+
+func _on_Area_mouse_entered():
+	$Control.visible = true
+	pass
+
+func _on_Area_mouse_exited():
+	$Control.visible = false
 	pass
--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -3,8 +3,10 @@ extends Spatial
 enum View { AERIAL, TOP, SIDE }
 
 export var speed: float = 300
+export var zoom_speed: float = 5
 
 var view = View.TOP
+var velocity = Vector3.ZERO
 
 func _ready():
 	pass
@@ -12,16 +14,29 @@ func _ready():
 func _physics_process(delta):
 	
 	# Moves the view around
-	var velocity = Vector3.ZERO
-	velocity.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	velocity.z = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	self.velocity.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	self.velocity.z = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	self.translate(velocity * self.speed * delta)
 	
 	pass
 
 func _unhandled_input(event):
-	if event.is_action_pressed("middle_button"):
+	if event.is_action_pressed('middle_button'):
 		self.change_view()
+		
+	if event is InputEventMouseButton:
+		self.zoom(event.button_index)
+	
+	pass
+
+# Changes the zoom of the camera
+func zoom(button):
+	if self.view == View.TOP:
+		if button == BUTTON_WHEEL_UP: self.translate(Vector3.DOWN * zoom_speed)
+		if button == BUTTON_WHEEL_DOWN: self.translate(Vector3.UP * zoom_speed)
+	if self.view == View.SIDE:
+		if button == BUTTON_WHEEL_UP: $Camera.size -= zoom_speed
+		if button == BUTTON_WHEEL_DOWN: $Camera.size += zoom_speed
 	pass
 
 # Changes the current viewport
